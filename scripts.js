0883269f465d936bc518bba0bedc0818ec71aa1f
Eduardo <eduardo.r.sampaoi@gmail.com>
2026-05-08 11:24:44 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -13,3 +13,42 @@ function mostrarSecao(idSecao){
     }
 }
 
+//Validação do formulário
+
+const formulario = document.getElementById('meu-formulario');
+const feedback = document.getElementById('feedback');
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+
+    if(nome === ""){
+        exibirFeedback("Por favor, digite seu nome para continuar.", "erro");
+        return;
+    } 
+
+    if(email === ""){
+        exibirFeedback("Por favor, digite seu e-mail para continuar.", "erro");
+        return;
+    }
+
+    if(mensagem.length < 10){
+        exibirFeedback("Sua mensagem precisa ter pelo menos 10 caracteres.", "erro");
+        return;
+    }
+
+    exibirFeedback("Mensagem enviada com sucesso! 🚀", "sucesso");
+    formulario.reset();
+})
+
+function exibirFeedback(texto, tipo) {
+    feedback.innerText = texto;
+    feedback.className = `feedback-ativo ${tipo}`; 
+
+    setTimeout(() => {
+        feedback.className = "feedback-oculto";
+    }, 4000);
+}
